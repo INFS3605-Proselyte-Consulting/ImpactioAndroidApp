3fb2ec4ec4605aa1ef1626ed68ad531045ce92b5
BeatriceLee01 <qiaoyinglee@outlook.com>
2022-07-09 16:19:38 +1000

    Added handle for view elements for ProfileActivity.java

--- app/src/main/java/com/example/impactioproselyteconsulting/ProfileActivity.java
+++ app/src/main/java/com/example/impactioproselyteconsulting/ProfileActivity.java
@@ -3,12 +3,28 @@ package com.example.impactioproselyteconsulting;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class ProfileActivity extends AppCompatActivity {
+    private static final String TAG = "ProfileActivity";
+    public static final String INTENT_MESSAGE = "intent_message";
+    private TextView tvName, tvBiography, tvImpactioRole, tvCompany, tvPosition, tvEmail, tvLinkedIn;
+    private ImageView ivProfilePic;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
+
+        // Get handle for view elements
+        ivProfilePic = findViewById(R.id.ivProfilePic);
+        tvName = findViewById(R.id.profileName);
+        tvBiography = findViewById(R.id.tvBiography);
+        tvImpactioRole = findViewById(R.id.tvImpactioRole);
+        tvCompany = findViewById(R.id.tvCompany);
+        tvPosition = findViewById(R.id.tvPosition);
+        tvEmail = findViewById(R.id.tvEmail);
+        tvLinkedIn = findViewById(R.id.tvLinkedin);
     }
 }
