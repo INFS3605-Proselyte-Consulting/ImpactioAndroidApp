965f3e3a894dafe545a382fe3587356831b0fbdb
BeatriceLee01 <qiaoyinglee@outlook.com>
2022-07-17 00:50:48 +1000

    Added cardview into profile page

--- app/src/main/java/com/example/impactioproselyteconsulting/ProfileActivity.java
+++ app/src/main/java/com/example/impactioproselyteconsulting/ProfileActivity.java
@@ -17,7 +17,7 @@ import com.google.android.material.bottomnavigation.BottomNavigationView;
 public class ProfileActivity extends AppCompatActivity {
     private static final String TAG = "ProfileActivity";
     public static final String INTENT_MESSAGE = "intent_message";
-    private TextView tvName, tvBiography, tvImpactioRole, tvCompany, tvPosition, tvEmail, tvLinkedIn;
+    private TextView tvName, tvBiography;
     private ImageView ivProfilePic;
     private Button btnEdit, btnLogOut;
 
@@ -55,24 +55,19 @@ public class ProfileActivity extends AppCompatActivity {
 
         // Get handle for view elements
         ivProfilePic = findViewById(R.id.ivProfilePic);
-        tvName = findViewById(R.id.profileName);
+        tvName = findViewById(R.id.tvName);
         tvBiography = findViewById(R.id.tvBiography);
-        tvImpactioRole = findViewById(R.id.tvImpactioRole);
-        tvCompany = findViewById(R.id.tvCompany);
-        tvPosition = findViewById(R.id.tvPosition);
-        tvEmail = findViewById(R.id.tvEmail);
-        tvLinkedIn = findViewById(R.id.tvLinkedin);
         btnLogOut = findViewById(R.id.btnLogOut);
 
-        btnEdit = findViewById(R.id.btnEdit);
-        btnEdit.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                // Launch the Discover Challenges Activity
-                Intent intent = new Intent(ProfileActivity.this, ProfileEditActivity.class);
-                startActivityForResult(intent, 101);
-            }
-        });
+//        btnEdit = findViewById(R.id.btnEdit);
+//        btnEdit.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                // Launch the Discover Challenges Activity
+//                Intent intent = new Intent(ProfileActivity.this, ProfileEditActivity.class);
+//                startActivityForResult(intent, 101);
+//            }
+//        });
     }
 
         // Filter and Produce selected SDGs
