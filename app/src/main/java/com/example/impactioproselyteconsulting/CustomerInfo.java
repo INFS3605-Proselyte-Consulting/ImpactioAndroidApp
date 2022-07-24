4ee505a8619679d15870723508430adf1887b6a6
Jetzer2232 <79190016+Jetzer2232@users.noreply.github.com>
2022-07-24 18:45:09 +1000

    Proved that realtime database connection does exist. Will need to tinker to properly connect login.

---
+++ app/src/main/java/com/example/impactioproselyteconsulting/CustomerInfo.java
@@ -0,0 +1,56 @@
+package com.example.impactioproselyteconsulting;
+
+public class CustomerInfo {
+    private String cusEmail, cusPass, cusName, cusBio, cusSDG, cusExpertise;
+
+    public CustomerInfo() {
+    }
+
+    public String getCusEmail() {
+        return cusEmail;
+    }
+
+    public void setCusEmail(String cusEmail) {
+        this.cusEmail = cusEmail;
+    }
+
+    public String getCusPass() {
+        return cusPass;
+    }
+
+    public void setCusPass(String cusPass) {
+        this.cusPass = cusPass;
+    }
+
+    public String getCusName() {
+        return cusName;
+    }
+
+    public void setCusName(String cusName) {
+        this.cusName = cusName;
+    }
+
+    public String getCusBio() {
+        return cusBio;
+    }
+
+    public void setCusBio(String cusBio) {
+        this.cusBio = cusBio;
+    }
+
+    public String getCusSDG() {
+        return cusSDG;
+    }
+
+    public void setCusSDG(String cusSDG) {
+        this.cusSDG = cusSDG;
+    }
+
+    public String getCusExpertise() {
+        return cusExpertise;
+    }
+
+    public void setCusExpertise(String cusExpertise) {
+        this.cusExpertise = cusExpertise;
+    }
+}
--- app/src/main/java/com/example/impactioproselyteconsulting/RegisterActivity.java
+++ app/src/main/java/com/example/impactioproselyteconsulting/RegisterActivity.java
@@ -2,6 +2,7 @@ package com.example.impactioproselyteconsulting;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -15,6 +16,11 @@ import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class RegisterActivity extends AppCompatActivity {
 
@@ -22,13 +28,34 @@ public class RegisterActivity extends AppCompatActivity {
     public static final String INTENT_MESSAGE = "intent_message";
     private EditText et_Email, et_Password, et_Fullname;
     private Button btn_Register;
+    // creating a variable for our
+    // Firebase Database.
+    FirebaseDatabase firebaseDatabase;
+
+    // creating a variable for our Database
+    // Reference for Firebase.
+    DatabaseReference databaseReference;
+
+    // creating a variable for
+    // our object class
+    CustomerInfo customerInfo;
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
         setContentView(R.layout.activity_register);
-
         setTitle("Register");
+
+        // below line is used to get the
+        // instance of our FIrebase database.
+        firebaseDatabase = FirebaseDatabase.getInstance();
+
+        // below line is used to get reference for our database.
+        databaseReference = firebaseDatabase.getReference("CustomerInfo");
+
+        // initializing our object
+        // class variable.
+        customerInfo = new CustomerInfo();
 
         Toast.makeText(RegisterActivity.this, "You can register now", Toast.LENGTH_LONG).show();
 
@@ -36,19 +63,32 @@ public class RegisterActivity extends AppCompatActivity {
         et_Password = findViewById(R.id.ptPassword);
         et_Fullname = findViewById(R.id.ptFullname);
 
+
+
         btn_Register = findViewById(R.id.bRegister);
         btn_Register.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 String textEmail = et_Email.getText().toString();
                 String textPassword = et_Password.getText().toString();
-                String textFullname = et_Fullname.getText().toString();
-                registerUser(textFullname,textEmail,textPassword);
+                String textName = et_Fullname.getText().toString();
+                if (TextUtils.isEmpty(textEmail) && TextUtils.isEmpty(textPassword) && TextUtils.isEmpty(textName)) {
+                    // if the text fields are empty
+                    // then show the below message.
+                    Toast.makeText(RegisterActivity.this, "Please add some data.", Toast.LENGTH_SHORT).show();
+                } else {
+                    // else call the method to add
+                    // data to our database.
+                    registerUser(textName,textEmail,textPassword);
+                }
             }
         });
 
     }
-    private void registerUser(String textFullname, String textEmail, String textPassword){
+    private void registerUser(String textName, String textEmail, String textPassword){
+        customerInfo.setCusName(textName);
+        customerInfo.setCusEmail(textEmail);
+        customerInfo.setCusPass(textPassword);
         FirebaseAuth auth = FirebaseAuth.getInstance();
         auth.createUserWithEmailAndPassword(textEmail,textPassword).addOnCompleteListener(RegisterActivity.this, new OnCompleteListener<AuthResult>() {
             @Override
@@ -56,10 +96,25 @@ public class RegisterActivity extends AppCompatActivity {
                 if(task.isSuccessful()){
                     Toast.makeText(RegisterActivity.this,"User Registered",Toast.LENGTH_LONG).show();
 
+                    databaseReference.addValueEventListener(new ValueEventListener() {
+                        @Override
+                        public void onDataChange(@NonNull DataSnapshot snapshot) {
+                            databaseReference.setValue(customerInfo);
+                            Toast.makeText(RegisterActivity.this,"data added",Toast.LENGTH_SHORT).show();
+                        }
+
+                        @Override
+                        public void onCancelled(@NonNull DatabaseError error) {
+                            Toast.makeText(RegisterActivity.this,"Fail to add data " + error,Toast.LENGTH_LONG).show();
+                        }
+                    });
+
                     //Open user Profile
                     Intent intent = new Intent(RegisterActivity.this,MainActivity.class);
                     startActivity(intent);
                     finish();
+                } else {
+                    Toast.makeText(RegisterActivity.this,"Registration Failed Lol",Toast.LENGTH_LONG).show();
                 }
             }
         });
