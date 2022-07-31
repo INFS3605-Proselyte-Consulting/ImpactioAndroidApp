52bb301db832dd4d0851d83b904df3eb344584e1
adrerid <Benjamin.Fu2412@Outlook.com>
2022-08-01 00:09:17 +1000

    mainactivity has animations

--- app/src/main/java/com/example/impactioproselyteconsulting/MainActivity.java
+++ app/src/main/java/com/example/impactioproselyteconsulting/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.impactioproselyteconsulting;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
 
 import android.content.Intent;
 import android.os.Bundle;
@@ -12,11 +13,19 @@ import android.widget.Button;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 import android.view.MenuItem;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
 
     Animation animSlideIn;
+
+    Button bViewInProgress, bViewToDo, bViewCompleted,bGoToRating;
+    CardView SolutionsInProgressCardView, SolutionsToDoCardView, CompletedCardView;
+    TextView ToDoTV, InProgressTV, CompletedTV, solutionstv1, solutionstv2, solutionstv3;
+
+
+
 
 
     @Override
@@ -27,6 +36,37 @@ public class MainActivity extends AppCompatActivity {
 
         animSlideIn = android.view.animation.AnimationUtils.loadAnimation(getApplicationContext(),
                 R.anim.slide_in_bottom);
+
+
+
+        bViewInProgress = findViewById(R.id.bViewInProgress);
+        bViewInProgress.startAnimation(animSlideIn);
+        bViewToDo = findViewById(R.id.bViewToDo);
+        bViewToDo.startAnimation(animSlideIn);
+        bViewCompleted = findViewById(R.id.bViewCompleted);
+        bViewCompleted.startAnimation(animSlideIn);
+        bGoToRating = findViewById(R.id.bGoToRating);
+        bGoToRating.startAnimation(animSlideIn);
+        SolutionsInProgressCardView = findViewById(R.id.SolutionsInProgressCardView);
+        SolutionsInProgressCardView.startAnimation(animSlideIn);
+        SolutionsToDoCardView = findViewById(R.id.SolutionsToDoCardView);
+        SolutionsToDoCardView.startAnimation(animSlideIn);
+        CompletedCardView = findViewById(R.id.CompletedCardView);
+        CompletedCardView.startAnimation(animSlideIn);
+        ToDoTV = findViewById(R.id.ToDoTV);
+        ToDoTV.startAnimation(animSlideIn);
+        InProgressTV = findViewById(R.id.InProgressTV);
+        InProgressTV.startAnimation(animSlideIn);
+        CompletedTV = findViewById(R.id.CompletedTV);
+        CompletedTV.startAnimation(animSlideIn);
+        solutionstv1 = findViewById(R.id.solutionstv1);
+        solutionstv1.startAnimation(animSlideIn);
+        solutionstv2 = findViewById(R.id.solutionstv2);
+        solutionstv2.startAnimation(animSlideIn);
+        solutionstv3 = findViewById(R.id.solutionstv3);
+        solutionstv3.startAnimation(animSlideIn);
+
+
 
 
         // Initialize and assign variable
