caa6ed628cb67e8b260c521ff86507046d19769a
adrerid <Benjamin.Fu2412@Outlook.com>
2022-08-01 00:21:36 +1000

    AdminDashboardActivity has animations

--- app/src/main/java/com/example/impactioproselyteconsulting/AdminDashboardActivity.java
+++ app/src/main/java/com/example/impactioproselyteconsulting/AdminDashboardActivity.java
@@ -5,24 +5,62 @@ import android.os.Bundle;
 import android.view.View;
 import android.view.animation.Animation;
 import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
 
 public class AdminDashboardActivity extends AppCompatActivity {
     public Button viewProjects,viewCurators;
     Animation animSlideIn;
 
+    TextView welcomeTV, InProgressTV, solutionstv1, ToDoTV, solutionstv2;
+    Button bAdminViewProjects, bAdminViewCurators, bAdminLogout;
+    CardView allProjectsCV, AllCuratorsCV;
+
+
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
+
+
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_admin_dashboard);
 
         viewProjects = findViewById(R.id.bAdminViewProjects);
-        viewProjects.startAnimation(animSlideIn);
         viewCurators = findViewById(R.id.bAdminViewCurators);
+        welcomeTV = findViewById(R.id.welcomeTV);
+        InProgressTV = findViewById(R.id.InProgressTV);
+        solutionstv1 = findViewById(R.id.solutionstv1);
+        ToDoTV = findViewById(R.id.ToDoTV);
+        solutionstv2 = findViewById(R.id.solutionstv2);
+        bAdminViewProjects = findViewById(R.id.bAdminViewProjects);
+        bAdminViewCurators = findViewById(R.id.bAdminViewCurators);
+        bAdminLogout = findViewById(R.id.bAdminLogout);
+        allProjectsCV = findViewById(R.id.allProjectsCV);
+        AllCuratorsCV = findViewById(R.id.AllCuratorsCV);
+
+        animSlideIn = android.view.animation.AnimationUtils.loadAnimation(getApplicationContext(),
+                R.anim.slide_in_bottom);
+
+
+        viewProjects.startAnimation(animSlideIn);
         viewCurators.startAnimation(animSlideIn);
+        welcomeTV.startAnimation(animSlideIn);
+        InProgressTV.startAnimation(animSlideIn);
+        solutionstv1.startAnimation(animSlideIn);
+        ToDoTV.startAnimation(animSlideIn);
+        solutionstv2.startAnimation(animSlideIn);
+        bAdminViewProjects.startAnimation(animSlideIn);
+        bAdminViewCurators.startAnimation(animSlideIn);
+        bAdminLogout.startAnimation(animSlideIn);
+        allProjectsCV.startAnimation(animSlideIn);
+        AllCuratorsCV.startAnimation(animSlideIn);
+
+
+
 
         viewProjects.setOnClickListener(new View.OnClickListener() {
             @Override
